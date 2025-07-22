9c480f8f829559e42db418f96ac60f357d3007bd
Felipe Becker dos Santos <beckerfelipee@gmail.com>
2025-07-22 11:47:05 +0100

    Update roles-and-accesses.py

--- roles-and-accesses.py
+++ roles-and-accesses.py
@@ -3,19 +3,21 @@
 # Example of use:
 """
 admin_users = [
-    "12345678901234567",  # Example user ID
+    "123456789012345678",  # Example user ID
     # Add more user IDs here
 ]
+
+admin_role = "123456789012345678" # Example role ID
 """
 
 # List of user IDs with full admin access to all commands including admin-level commands (e.g., /ram set, /restart) 
-# If left empty, any user can use these commands—unless admin_roles is not empty.
+# If left empty, any user can use these commands—unless admin_role is not empty.
 admin_users = []
 
 # Role IDs allowed to use all commands including admin-level commands (e.g., /ram set, /restart) 
 # If left empty, any user can use these commands—unless admin_users is not empty.
-admin_roles = []
+admin_role = ""
 
 # Role IDs allowed to use only informational commands (e.g., /server, /ram get)
 #If leave blank, any user will have access to these commands
-info_roles = []
+info_role = ""
